f3635f50f95b68d1fa3718967b3d29f8d3e7aec8
Caroline <Cluce059@gmail.com>
2021-06-26 17:31:14 -0400

    initialize game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+
+function fight(){
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName); //got something im supposed to deliver, developers eyes only... well, got to go
+//fight();
+console.log("This logs a string, goo for leaving yourself a message");
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
