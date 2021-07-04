c05988450d554eb8ac0f60363dfc8cdbf7f72b0d
Caroline <Cluce059@gmail.com>
2021-07-03 23:39:09 -0400

    random attack order

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,43 +18,65 @@ var fightOrSkip = function(){
         }
     }
 };
-//fight function
-var fight = function(enemy){
-    console.log(enemy);
-    while(enemy.health> 0 && playerInfo.health > 0){
-        if(fightOrSkip()){
-            //if true, leave fight via break bc user chose to skip
-            break;
+
+var fight = function(enemy) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
+    // randomly change turn order
+    if (Math.random() > 0.5) {
+      isPlayerTurn = false;
+    }
+  
+    while (playerInfo.health > 0 && enemy.health > 0) {
+      if (isPlayerTurn) {
+        // ask player if they'd like to fight or skip using fightOrSkip function
+        if (fightOrSkip()) {
+          // if true, leave fight by breaking loop
+          break;
         }
-            //subtract playerAttack from enemyHealth
-            enemy.health = Math.max(0, enemy.health - playerInfo.attack);
-            console.log(playerInfo.name +" attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-            );
-        //check enemyHealth
-        if(enemy.health <= 0){
-            window.alert(enemy.name+ " has died!");
-            break;
+  
+        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+  
+        // remove enemy's health by subtracting the amount we set in the damage variable
+        enemy.health = Math.max(0, enemy.health - damage);
+        console.log( playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+        );
+  
+        // check enemy's health
+        if (enemy.health <= 0) {
+          window.alert(enemy.name + " has died!");
+  
+          // award player money for winning
+          playerInfo.money = playerInfo.money + 20;
+  
+          // leave while() loop since enemy is dead
+          break;
+        } else {
+          window.alert(enemy.name + " still has " + enemy.health + " health left.");
         }
-        else{
-            window.alert(enemy.name +" still has " + enemy.health + " health left.");
-        }
-        //subtract enemyAttack from playerHealth
-        var damage = randomNumber(enemy.attack -3, enemy.attack);
+        // player gets attacked first
+      } else {
+        var damage = randomNumber(enemy.attack - 3, enemy.attack);
+  
+        // remove player's health by subtracting the amount we set in the damage variable
         playerInfo.health = Math.max(0, playerInfo.health - damage);
         console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+          enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
         );
-        //check playerHealth
-        if(playerInfo.health <= 0){
-            window.alert(playerInfo.name + " has died!");
-            playerInfo.money = playerInfo.money + 20;
-            break;
+  
+        // check player's health
+        if (playerInfo.health <= 0) {
+          window.alert(playerInfo.name + " has died!");
+          // leave while() loop if player is dead
+          break;
+        } else {
+          window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
         }
-        else{
-            window.alert(playerInfo.name + " still has " + playerInfo.health +" health left.");
-        }
+      }
+      // switch turn order for next round
+      isPlayerTurn = !isPlayerTurn;
     }
-};
+  };
 //fight();
 // Game States
 // "WIN" - Player robot has defeated all enemy-robots
